bdd0438df299d3ae83354dc7a13b1fb8a25a4a23
Raghav2709 <152988951+Raghav2709@users.noreply.github.com>
2024-01-18 15:48:46 +0530

    changes made

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -64,7 +64,7 @@ import {
             height="auto"
             alt="post"
             style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-            src={picturePath}
+            src = {`https://sociobuddy-back.onrender.com/assets/${picturePath}`}
           />
         )}
         <FlexBetween mt="0.25rem">
